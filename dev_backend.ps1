d34e4bc3beead325b03c2c2ec17cd8b5b432a9dd
Devon Burriss <devon@chimplab.co>
2018-12-22 14:06:13 +0100

    Create dev_backend.ps1
    Workloads for typical backend .NET development

---
+++ dev_backend.ps1
@@ -0,0 +1,53 @@
+# Description: Boxstarter Script
+# Author: Microsoft
+# Common settings for web dev
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Setting up Windows ---
+executeScript "FileExplorerSettings.ps1";
+executeScript "SystemConfiguration.ps1";
+executeScript "CommonDevTools.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "HyperV.ps1";
+executeScript "Docker.ps1";
+executeScript "WSL.ps1";
+
+#--- Tools ---
+#--- Installing VS and VS Code with Git
+# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
+# visualstudio2017community
+# visualstudio2017professional
+# visualstudio2017enterprise
+
+choco install -y visualstudio2017professional --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+Update-SessionEnvironment #refreshing env due to Git install
+
+#--- .NET Workloads for backend development ---
+choco install -y visualstudio2017-workload-azure # Adds F# support :)
+choco install -y visualstudio2017-workload-manageddesktop
+choco install -y visualstudio2017-workload-netweb
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
